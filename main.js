3c9fc14746ef9aeb35dd8377dcff15278fca18bf
saim514 <81713694+saim514@users.noreply.github.com>
2022-02-16 18:46:21 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -23,3 +23,39 @@ function modelLoaded()
 {
     console.log("Model is loaded!");
 }
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data1 = "The prediction is" + prediction1;
+    var utterThis = new SpeechSynthesisUtterance(speak_data1);
+    utterThis.rate = 0.5;
+    synth.speak(utterThis);
+}
+
+function check()
+{
+    img = document.getElementById("captured_image");
+    classifier.classify(img , gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error){
+        console.error(error)
+    } else{
+        console.log(results);
+        document.getElementById("result_signal_name").innerHTML = results[0].label;
+        prediction1 = results[0].label;
+        speak();
+        if(results[0].label == "good"){
+            document.getElementById("update_signal").innerHTML = "&#128077;"
+        }
+        if(results[0].label == "peace"){
+            document.getElementById("update_signal").innerHTML = "&#9996;"
+        }
+        if(results[0].label == "okay"){
+            document.getElementById("update_signal").innerHTML = "&#9996;"
+        } 
+    }
+}
